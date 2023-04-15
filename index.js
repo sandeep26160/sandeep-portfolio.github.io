34030e19fe6b7e8d96d2c4d0cff74d5cb8b6923e
SANDEEP KUMAR YADAV <sandeepyadav26160@gmail.com>
2023-04-15 12:21:08 +0530

    my portfolio website

---
+++ index.js
@@ -0,0 +1,7 @@
+/* ===================== typing animation ===========================*/
+var typed = new Typed(".typing",{
+    String:["Web Developer","Backend Developer","Frontend Developer","Programmer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
